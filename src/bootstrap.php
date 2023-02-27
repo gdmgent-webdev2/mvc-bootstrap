07bf43a435dbfbcbfb50cda26a8c41e39a97e71b
Frederick Roegiers <frederick@rogerthat.be>
2023-02-27 15:14:28 +0100

    wip

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,10 +2,15 @@
 
 use Slim\Factory\AppFactory;
 use Illuminate\Database\Capsule\Manager as Capsule;
-
+use Dotenv\Dotenv as Dotenv;
 
 // include autoload file, only once
 $loader = require_once BASEPATH . 'vendor/autoload.php';
+
+// load environment variables
+$dotenv = Dotenv::createImmutable(BASEPATH);
+$dotenv->load();
+
 
 // i like nice errors and i cannot lie
 $whoops = new \Whoops\Run();
@@ -18,9 +23,9 @@ $capsule = new Capsule;
 $capsule->addConnection([
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'game_wishlist',
-    'username' => 'root',
-    'password' => 'secret',
+    'database' => $_ENV['DATABASE_NAME'] ?? '',
+    'username' => $_ENV['DATABASE_USER'] ?? '',
+    'password' => $_ENV['DATABASE_PASSWORD'] ?? '',
     'port' => '3306',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
